ee707ecc82c5b9ba42e354fb29e0e613729e316a
Robin Ward <robin.ward@gmail.com>
2020-10-27 14:54:38 -0400

    REFACTOR: Test assertions should be imported.
    Previously they were global functions.

--- app/assets/javascripts/discourse/tests/setup-tests.js
+++ app/assets/javascripts/discourse/tests/setup-tests.js
@@ -16,6 +16,7 @@ import DiscourseURL from "discourse/lib/url";
 import {
   resetSite,
   applyPretender,
+  exists,
 } from "discourse/tests/helpers/qunit-helpers";
 import PreloadStore from "discourse/lib/preload-store";
 import User from "discourse/models/user";
@@ -77,6 +78,18 @@ export default function setupTests(app, container) {
         }
       );
       return window.sinon;
+    },
+  });
+  Object.defineProperty(window, "exists", {
+    get() {
+      deprecated(
+        "Accessing the global function `exists` is deprecated. Import it instead.",
+        {
+          since: "2.6.0.beta.4",
+          dropFrom: "2.6.0",
+        }
+      );
+      return exists;
     },
   });
 
